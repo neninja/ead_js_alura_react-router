98e7c5357dc304e11e565c7b8493f8e352fbe41b
Felipe Silva <felipe.dacs@gmail.com>
2021-06-03 16:23:39 -0300

    :tv: add aula 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
 
 function App() {
   return (
-  <>
-
-  </>
+  <Router>
+    <Route path='/'>
+      <Home />
+    </Route>
+    <Route path='/sobre'>
+      <Sobre />
+    </Route>
+  </Router>
   )
 }
 
