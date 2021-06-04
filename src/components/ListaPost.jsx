49047c2f42059665781e877fc81dabde556e968f
Felipe Silva <felipe.dacs@gmail.com>
2021-06-04 19:16:56 -0300

    :tv: add aula 7

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { busca } from '../api/api'
 
-export function ListaPost() {
+export function ListaPost({ url }) {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    busca(url, setPosts)
+  }, [])
+
   return (
     <section className="posts container">
+      {
+        posts.map((post) => (
+          <Link className={`cartao-post cartao-post--${post.categoria}`}>
+          <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p className="cartao-post__meta">{post.metadescription}</p>
+          </article>
 
+          </Link>
+        ))
+      }
     </section>
   )
 }
