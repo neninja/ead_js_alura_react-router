06392de6ff9a964fb7e271894ea26e21e5cd2edf
Felipe Silva <felipe.dacs@gmail.com>
2021-06-04 19:35:36 -0300

    :tv: add aula 9

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { busca } from '../api/api'
 import '../assets/css/post.css'
 
 export function Post() {
+  let history = useHistory()
   const { id } = useParams()
   const [ post, setPost ] = useState({})
 
   useEffect(() => {
     busca(`/posts/${id}`, setPost)
-  }, [id])
+      .catch(() => {
+        history.push('/404')
+      })
+  }, [id, history])
 
   return (
     <main className="container flex flex--centro">
